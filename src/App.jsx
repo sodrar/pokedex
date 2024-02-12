658aef2c7b8ff34a325fda9b9bd802898139f2c8
Sofian BOUDRAA <draaboufianso@gmail.com>
2024-02-12 15:11:02 +0100

    Structure en place, la connexion à l'API pokebuild fonctionne

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    const fetchPokes = async () => {
+      const reponse = await fetch("https://pokebuildapi.fr/api/v1/pokemon")
+      const jsonPokes = await reponse.json()
+      setPokemons(jsonPokes)
+      console.log(jsonPokes)
+    }
+    fetchPokes()
+  },[])
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test</h1>
     </>
   )
 }
